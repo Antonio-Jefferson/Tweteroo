66c3d2e6533e7746009e20bf93feb31358ee0ba7
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-01-05 12:03:22 -0300

    resquisição GET emplementada, retornando os 10 ultimos tweets feitos, projeto finalizado sem validação de dados ainda...

--- src/app.js
+++ src/app.js
@@ -9,22 +9,27 @@ const users = [];
 const tweetsUser = [];
 
 server.post("/sign-up", (req, res) => {
-    const { username, avatar} = req.body;
-    const newUser = {username, avatar};
+    const { username, avatar } = req.body;
+    const newUser = { username, avatar };
     users.push(newUser)
     res.send("OK")
 })
 
-server.post("/tweets", (req, res)=>{
-    const {username, tweet } = req.body;
+server.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
     if (!users.find(user => user.username === username)) {
         return res.status(400).send('UNAUTHORIZED');
     }
-    const newTweet = {username, tweet};
+    const { avatar } = users.find((user) => user.username === username)
+    const newTweet = { username, tweet, avatar };
     tweetsUser.push(newTweet);
     res.send("OK")
 })
 
+server.get("/tweets", (req, res) => {
+    let lastTweets = tweetsUser[tweetsUser.length - 10];
+    res.status(200).send(lastTweets)
+})
 
 const PORT = 5000;
-server.listen(PORT, () => console.log(`Server rodando na porta ${PORT} `))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
